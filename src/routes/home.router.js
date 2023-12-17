aa772e5b17c929baee02b0bb133c8078d4bc8183
Karlito <38817922+Karlito16@users.noreply.github.com>
2023-12-17 16:30:09 +0100

    Dev 1 (#1)
    * Initial project setup. Home page template. About page template. Navbar.
    
    * Camera native API integration.
    
    * Manifest. Icons.

---
+++ src/routes/home.router.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+router.get("/", async(req, res) => {
+    res.redirect("/home");
+});
+
+
+router.get("/home", async(req, res) => {
+    res.render("home", {
+    });
+});
+
+
+router.get("/about", async(req, res) => {
+   res.render("about", {
+   }) 
+});
+
+
+module.exports = {
+    path: "/",
+    router: router
+};
